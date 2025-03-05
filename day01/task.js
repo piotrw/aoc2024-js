b8cd92d771cf26c12e53ad02d65dcfc3e09b3e22
Piotrek Wysocki <garry@aikido.art.pl>
2025-03-05 23:18:07 +0100

    day01 - part 2

--- day01/task.js
+++ day01/task.js
@@ -1,15 +1,23 @@
 /*
 https://adventofcode.com/2024/day/1
  */
-import fs from 'fs';
+import fs from "fs";
 
-console.log('Day 01 / Task 01');
+console.log("Day 01 / Task 01\n");
 
-// const data = fs.readFileSync('./day01/demo.txt', 'utf-8').trim().split('\r\n');
-const data = fs.readFileSync('./day01/input.txt', 'utf-8').trim().split('\r\n');
+const mode = !!process.argv[2] && process.argv[2] === 'input' ? 'input' : 'demo';
+const file = `./day01/${mode}.txt`;
+
+if (!fs.existsSync(file)) {
+    console.error(`File ${file} not exists`);
+    process.exit(1);
+}
+
+const data = fs.readFileSync(file, 'utf-8').trim().split('\r\n');
 
 const left = data.map((line) => line.split('   ')[0]);
 const right = data.map((line) => line.split('   ')[1]);
+
 const task1 = (left, right) => {
     const sortedLeft = left.slice().sort();
     const sortedRight = right.slice().sort();
@@ -19,10 +27,25 @@ const task1 = (left, right) => {
 
 const task2 = (left, right) => {
 
-    return 0;
+    // count values
+    let count = new Map();
+    right.forEach((value) => {
+        if (!count.has(value)) count.set(value, 0);
+        count.set(value, count.get(value) + 1);
+    });
+
+    let sum = 0;
+    left.forEach((value) => {
+        const score = count.get(value) ?? 0;
+        sum += value * score;
+    })
+
+    return sum;
 }
 
-console.log({
-    result1: task1(left, right),
-    result2: task2(left, right),
-});
+console.log("Result 1: ", task1(left, right));
+console.log("Result 2: ", task2(left, right));
+
+if (mode === 'demo') {
+    console.info("\nDemo mode. Run task with argument input");
+}
